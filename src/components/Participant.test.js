ba54385c198e309c6c4edd6b7190c26fcc1684aa
aljazskrinjar <aljaz.skrinjar@gmail.com>
2018-01-10 11:56:23 +0100

    added more test for component participant

--- src/components/Participant.test.js
+++ src/components/Participant.test.js
@@ -12,9 +12,16 @@ describe('<Participant />', () => {
   const data = require('../data')
   const container = shallow(<Participant key = {0} { ... data.SportList[0].GoldMedalList[0] } />)
 
-  it('it is wrapped in a div with className', () => {
+  it('it is wrapped in tr', () => {
     expect(container).to.have.tagName('tr')
   })
 
-  
+  it('check if text matches', () => {
+    expect(container.find('td').at(0)).to.have.text( data.SportList[0].GoldMedalList[0].Participant.c_Participant )
+    expect(container.find('td').at(1)).to.have.text( data.SportList[0].GoldMedalList[0].Event.c_Name )
+    expect(container.find('td').at(2)).to.have.text( data.SportList[0].GoldMedalList[0].Gender.c_Name )
+  })
+
+
+
 })
